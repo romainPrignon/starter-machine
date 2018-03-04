99b79b1481322d6add67a1178c47d4dd5f326d97
romainprignon <pro.rprignon@gmail.com>
2018-03-04 18:01:23 +0100

    feat: ssh

--- framework.js
+++ framework.js
@@ -1,32 +1,100 @@
 const { isArray } = require('util')
 
 const {shellSync, shell} = require('execa')
+const {Client} = require('ssh2')
 
 const isGenerator = (fn) => fn.constructor.name === 'GeneratorFunction'
 const isAsync = (fn) => fn.constructor.name === 'AsyncFunction'
 
 const process = async (cmd, options = {}) => {
     if (isArray(cmd)) {
-        return await Promise.all(cmd.map(c => shell(c, options)))
+        return await Promise.all(cmd.map(async(c) => {
+            if (options.ssh) {
+                const res = await options.ssh(c)
+
+                return {
+                    stdout: res,
+                    stderr: '',
+                    code: 0,
+                    failed: false,
+                    signal: null,
+                    cmd: c,
+                    timedOut: false
+                }
+            }
+
+            const res = shell(c, options)
+            return res
+        }))
     }
-    return shellSync(cmd, options)
+
+    if (options.ssh) {
+        const res = await options.ssh(cmd)
+
+        return {
+            stdout: res,
+            stderr: '',
+            code: 0,
+            failed: false,
+            signal: null,
+            cmd,
+            timedOut: false
+        }
+    }
+
+    const res = shellSync(cmd, options)
+    return res
+}
+
+const handleSSH = (host, port, username, privateKey) => async (cmd) => {
+    return new Promise((resolve, reject) => {
+        const conn = new Client()
+
+        conn.on('ready', function() {
+            conn.exec(cmd, function(err, stream) {
+                if (err) throw err;
+
+                stream
+                    .on('close', function(code, signal) {
+                        // console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+                        conn.end();
+                    })
+                    .on('data', function(data) {
+                        // console.log(`${data}`)
+                        return resolve(data)
+                    })
+                    .stderr.on('data', function(data) {
+                        // console.error(data);
+                        throw data
+                    })
+            })
+        }).connect({
+            host,
+            port,
+            username,
+            privateKey
+        })
+    })
 }
 
 // @TODO
 // handle multiline command verbose
 // stream cmd output
+// parallel generator
 const framework = (options) => async (fn, ...args) => {
+    const ssh = options.ssh && handleSSH('35.198.126.29', 22, 'romainprignon', require('fs').readFileSync('/home/romainprignon/.ssh/google_compute_engine'))
+
     if (!isGenerator(fn)) {
         const cmd = await fn(...args)
 
         if (options.verbose) console.log(`➜ ${cmd}`)
 
         if (isArray(cmd)) {
-            const res = await process(cmd, {env: options.env})
+            const res = await process(cmd, {env: options.env, ssh})
             if (options.output) res.map(r => console.log(`• ${r.stdout}`))
         } else {
-            const res = await process(cmd, {env: options.env})
-            if (options.output) console.log(`• ${res.stdout}`)
+            const res = await process(cmd, {env: options.env, ssh})
+            if (options.output && res.stdout) console.log(`• ${res.stdout}`)
         }
 
         return 0
